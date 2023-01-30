773572671db8131fb2a1680934ed4fbb941b6e87
Alexandr <headofthefather@gmail.com>
2023-01-30 10:21:26 +0300

    Add 1 more line

--- Results/Program.cs
+++ Results/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] strings = { "You", "Hello", "the","World", "morethen3","23461", "bes", "t!!","stillmore" }; 
+﻿string[] strings = { "You", "'re", "Hello", "the","World", "morethen3","23461", "bes", "t!!","stillmore" }; 
 var result = strings.Where(s => s.Length <= 3).ToArray(); 
 Console.Write("["); 
 Console.Write(string.Join(", ", result)); 
