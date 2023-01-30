b1359b4868b8cd6bcc9692960e6c7f9e98092b4f
Alexandr <headofthefather@gmail.com>
2023-01-30 08:25:10 +0300

    Result Output

--- Results/Program.cs
+++ Results/Program.cs
@@ -1,2 +1,6 @@
 ﻿string[] strings = { "You", "Hello", "the","World", "morethen3","23461", "bes", "t!!","stillmore" }; 
 var result = strings.Where(s => s.Length <= 3).ToArray(); 
+Console.Write("["); 
+Console.Write(string.Join(", ", result)); 
+Console.Write("]"); 
+Console.ReadLine();
